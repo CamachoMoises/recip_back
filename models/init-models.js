31b73db6324da69acf5322fc0ef9f02d64c87860
Moises <35911832+CamachoMoises@users.noreply.github.com>
2024-10-22 07:29:06 -0400

    update: minor fix

--- models/init-models.js
+++ models/init-models.js
@@ -34,104 +34,38 @@ function initModels(sequelize) {
 
 	// group.belongsToMany(user, { as: 'user_id_users', through: user_group, foreignKey: "group_id", otherKey: "user_id" });
 	// user.belongsToMany(group, { as: 'group_id_groups', through: user_group, foreignKey: "user_id", otherKey: "group_id" });
-	course_days.belongsTo(course, {
-		as: 'course',
-		foreignKey: 'course_id',
-	});
-	course.hasMany(course_days, {
-		as: 'course_days',
-		foreignKey: 'course_id',
-	});
-	subject.belongsTo(course, {
-		as: 'course',
-		foreignKey: 'course_id',
-	});
-	course.hasMany(subject, { as: 'subject', foreignKey: 'course_id' });
-	subject_days.belongsTo(course_days, {
-		as: 'course_day',
-		foreignKey: 'course_days_id',
-	});
-	course_days.hasMany(subject_days, {
-		as: 'subject_days',
-		foreignKey: 'course_days_id',
-	});
-	subject_days.belongsTo(course_days, {
-		as: 'course_days_course',
-		foreignKey: 'course_days_course_id',
-	});
-	course_days.hasMany(subject_days, {
-		as: 'course_days_course_subject_days',
-		foreignKey: 'course_days_course_id',
-	});
+	// course_days.belongsTo(course, { as: "course", foreignKey: "course_id"});
+	// course.hasMany(course_days, { as: "course_days", foreignKey: "course_id"});
+	// subject.belongsTo(course, { as: "course", foreignKey: "course_id"});
+	// course.hasMany(subject, { as: "subject", foreignKey: "course_id"});
+	// subject_days.belongsTo(course_days, { as: "course_day", foreignKey: "course_days_id"});
+	// course_days.hasMany(subject_days, { as: "subject_days", foreignKey: "course_days_id"});
+	// subject_days.belongsTo(course_days, { as: "course_days_course", foreignKey: "course_days_course_id"});
+	// course_days.hasMany(subject_days, { as: "course_days_course_subject_days", foreignKey: "course_days_course_id"});
 	// group_permision.belongsTo(group, { as: "group", foreignKey: "group_id"});
 	// group.hasMany(group_permision, { as: "group_permission", foreignKey: "group_id"});
 	// user_group.belongsTo(group, { as: "group", foreignKey: "group_id"});
 	// group.hasMany(user_group, { as: "user_groups", foreignKey: "group_id"});
-	rating.belongsTo(instructor, {
-		as: 'instructor',
-		foreignKey: 'instructor_id',
-	});
-	instructor.hasMany(rating, {
-		as: 'ratings',
-		foreignKey: 'instructor_id',
-	});
+	// rating.belongsTo(instructor, { as: "instructor", foreignKey: "instructor_id"});
+	// instructor.hasMany(rating, { as: "ratings", foreignKey: "instructor_id"});
 	// permisions.belongsTo(module, { as: "module", foreignKey: "module_id"});
-	module.hasMany(permisions, {
-		as: 'permisions',
-		foreignKey: 'module_id',
-	});
+	// module.hasMany(permisions, { as: "permisions", foreignKey: "module_id"});
 	// group_permision.belongsTo(permisions, { as: "permision", foreignKey: "permission_id"});
 	// permisions.hasMany(group_permision, { as: "group_permisions", foreignKey: "permission_id"});
 	// user_permision.belongsTo(permisions, { as: "permision", foreignKey: "permisions_id"});
 	// permisions.hasMany(user_permision, { as: "user_permisions", foreignKey: "permisions_id"});
-	rating.belongsTo(student, {
-		as: 'student',
-		foreignKey: 'student_id',
-	});
-	student.hasMany(rating, {
-		as: 'ratings',
-		foreignKey: 'student_id',
-	});
-	subject_days.belongsTo(subject, {
-		as: 'subject',
-		foreignKey: 'subject_id',
-	});
-	subject.hasMany(subject_days, {
-		as: 'subject_days',
-		foreignKey: 'subject_id',
-	});
-	rating.belongsTo(subject_days, {
-		as: 'subject_day',
-		foreignKey: 'subject_days_id',
-	});
-	subject_days.hasMany(rating, {
-		as: 'ratings',
-		foreignKey: 'subject_days_id',
-	});
-	rating.belongsTo(subject_days, {
-		as: 'subject_days_subject',
-		foreignKey: 'subject_days_subject_id',
-	});
-	subject_days.hasMany(rating, {
-		as: 'subject_days_subject_ratings',
-		foreignKey: 'subject_days_subject_id',
-	});
-	rating.belongsTo(subject_days, {
-		as: 'subject_days_course_day',
-		foreignKey: 'subject_days_course_days_id',
-	});
-	subject_days.hasMany(rating, {
-		as: 'subject_days_course_days_ratings',
-		foreignKey: 'subject_days_course_days_id',
-	});
-	rating.belongsTo(subject_days, {
-		as: 'subject_days_course_days_course',
-		foreignKey: 'subject_days_course_days_course_id',
-	});
-	subject_days.hasMany(rating, {
-		as: 'subject_days_course_days_course_ratings',
-		foreignKey: 'subject_days_course_days_course_id',
-	});
+	// rating.belongsTo(student, { as: "student", foreignKey: "student_id"});
+	// student.hasMany(rating, { as: "ratings", foreignKey: "student_id"});
+	// subject_days.belongsTo(subject, { as: "subject", foreignKey: "subject_id"});
+	// subject.hasMany(subject_days, { as: "subject_days", foreignKey: "subject_id"});
+	// rating.belongsTo(subject_days, { as: "subject_day", foreignKey: "subject_days_id"});
+	// subject_days.hasMany(rating, { as: "ratings", foreignKey: "subject_days_id"});
+	// rating.belongsTo(subject_days, { as: "subject_days_subject", foreignKey: "subject_days_subject_id"});
+	// subject_days.hasMany(rating, { as: "subject_days_subject_ratings", foreignKey: "subject_days_subject_id"});
+	// rating.belongsTo(subject_days, { as: "subject_days_course_day", foreignKey: "subject_days_course_days_id"});
+	// subject_days.hasMany(rating, { as: "subject_days_course_days_ratings", foreignKey: "subject_days_course_days_id"});
+	// rating.belongsTo(subject_days, { as: "subject_days_course_days_course", foreignKey: "subject_days_course_days_course_id"});
+	// subject_days.hasMany(rating, { as: "subject_days_course_days_course_ratings", foreignKey: "subject_days_course_days_course_id"});
 	// instructor.belongsTo(user, { as: "user", foreignKey: "user_id"});
 	// user.hasMany(instructor, { as: "instructors", foreignKey: "user_id"});
 	// student.belongsTo(user, { as: "user", foreignKey: "user_id"});
