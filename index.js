8d0a5ef128c80294cf39060e765503c41d7bbb68
Moises Camacho <moisescamacho26@gmail.com>
2024-10-29 19:26:48 -0400

    Update: minor changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function assertDbConnection() {
 async function init() {
 	try {
 		await assertDbConnection();
-		await sequelize.sync({ force: false });
+		await sequelize.sync({ force: true });
 		// Ruta básica para la raíz
 		app.get('/', (req, res) => {
 			res.send('¡Hola, mundo desde Express!!!!!');
